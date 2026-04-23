3c1107aed47b8dd16ec22f05846eb6efab0a823d
Abhishek Pal <palabhishek085@gmail.com>
2026-04-23 22:45:52 +0530

    Basics Revision

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId=14453
+// accountId=2
+// console.log(accountId);
+
+let accountEmail="palabhishek085@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+accountEmail="hii@test.com"
+accountPassword="34"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
