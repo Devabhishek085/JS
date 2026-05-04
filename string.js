7fbc0a8b32bb126ed329649a704587fae45e009b
Abhishek Pal <palabhishek085@gmail.com>
2026-05-04 23:36:14 +0530

    Strings

---
+++ string.js
@@ -0,0 +1,16 @@
+const gameName=new String ('Abhishekha')
+console.log(gameName);
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString =gameName.slice(-8,4)
+console.log(anotherString);
+
+const firString="   Abhishek    "
+// firString.trim();
+console.log(firString.trim());
+
+const url="https://abhishek.com/abhishek%20pal"
+console.log( url.replaceAll("h","-"));
+
+
--- variables.js
+++ variables.js
@@ -1,11 +1,17 @@
-const accountId=14453
-// accountId=2
-// console.log(accountId);
+// const accountId=14453
+// // accountId=2
+// // console.log(accountId);
 
-let accountEmail="palabhishek085@gmail.com"
-var accountPassword="12345"
-accountCity="Jaipur"
-accountEmail="hii@test.com"
-accountPassword="34"
+// let accountEmail="palabhishek085@gmail.com"
+// var accountPassword="12345"
+// accountCity="Jaipur"
+// accountEmail="hii@test.com"
+// accountPassword="34"
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// // Print each element in the array
+// let arr = [accountId, accountEmail, accountPassword, accountCity];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
