3379bf8ffed1a8a7eafcf9e50ccf136f14469ed2
Abhishek Pal <palabhishek085@gmail.com>
2026-05-06 22:55:54 +0530

    switch_statement

---
+++ switch.js
@@ -0,0 +1,20 @@
+// const month=2
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         break;
+// }
+
+//condition ? truth : false
+
+const price=200;
+price>=100?console.log("greater than 100"):console.log("equal to 200");
+
