31671c4c34841f25101b28ae0c45cace19cd5018
Abhishek Pal <palabhishek085@gmail.com>
2026-05-06 23:45:49 +0530

    for of  and for each loops

---
+++ forOfLoop.js
@@ -0,0 +1,12 @@
+// const arr=[1,2,3,4,5]
+// for (const element of arr) {
+//     console.log(element);  
+// }
+
+const coding=["js","ruby","java","python","cpp"]
+// coding.forEach(function(items){
+//     console.log(items+1);
+// })
+coding.forEach((dets)=>{
+    console.log(dets);
+})
