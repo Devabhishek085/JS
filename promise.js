7f0c22d484eb57ec74062505e68dc26229e5ad87
Abhishek Pal <palabhishek085@gmail.com>
2026-05-10 23:00:30 +0530

    fetch url

--- promise.js
+++ promise.js
@@ -24,35 +24,46 @@
 //     console.log(user);
 // })
 
-const PromiseFour=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error=true
-        if (!error) {
-            resolve({username: "Abhishek", pass: "123"})
-        }
-        else{
-            reject("ERROR : Something went wrong")
-        }
-    },2000)
+// const PromiseFour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=true
+//         if (!error) {
+//             resolve({username: "Abhishek", pass: "123"})
+//         }
+//         else{
+//             reject("ERROR : Something went wrong")
+//         }
+//     },2000)
+// })
+// PromiseFour.then(function(user){
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username); 
+// }).catch((error) => {
+//     console.log("Error");
+// })
+
+
+// async function getAllUsers(){
+//     try {
+//         const response=await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data=await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E : ",error);
+        
+//     }
+// }
+// getAllUsers()
+
+fetch("https://api.github.com/users/Devabhishek085")
+.then((response)=>{
+    return response.json()
 })
-PromiseFour.then(function(user){
-    console.log(user);
-    return user.username
-}).then((username) => {
-    console.log(username); 
-}).catch((error) => {
+.then((data)=>{
+    console.log(data);
+})
+.catch(()=>{
     console.log("Error");
 })
-
-
-async function getAllUsers(){
-    try {
-        const response=await fetch("https://jsonplaceholder.typicode.com/users")
-        const data=await response.json()
-        console.log(data);
-    } catch (error) {
-        console.log("E : ",error);
-        
-    }
-}
-getAllUsers()
